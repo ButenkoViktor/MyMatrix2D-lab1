0d1213808b34bcf2d92576e23e68aca716598ff1
Viktor Butenko <viktor.butenko@microsoft.wsei.edu.pl>
2025-03-20 10:49:59 +0100

    Zadanie 1-4

---
+++ Matrix2d/Matrix2dLib/matrix2d.cs
@@ -0,0 +1,35 @@
+﻿namespace Matrix2dLib
+{
+    public class Matrix2d
+    {
+        int a, b, c, d; // private fields
+        /*
+        -----
+        |a b|
+        |c d|
+        -----
+        */
+        // Constructor
+        public Matrix2d(int a, int b, int c, int d)
+        {
+            this.a = a; this.b = b; this.c = c; this.d = d;
+        }
+        // konstruktor bezparametrowy
+        public Matrix2d() : this(1, 0, 0, 1) { }
+
+        //publiczne stałe
+        public static  Matrix2d Id
+        {
+            get { return new Matrix2d(); }
+        }
+
+        //macierz zerowa
+        public static  Matrix2d Zero
+        {
+            get { return new Matrix2d(0, 0, 0, 0); }
+        }
+
+    }
+}
+
+
