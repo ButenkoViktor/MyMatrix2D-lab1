5efea550b7db2c1fe4b1d05d4c066b5d5a3e6266
Viktor Butenko <viktor.butenko@microsoft.wsei.edu.pl>
2025-03-20 11:28:26 +0100

    Zadanie 1-8

--- Matrix2d/Matrix2dConsoleapp/Program.cs
+++ Matrix2d/Matrix2dConsoleapp/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Matrix2dLib;
+var m = new Matrix2d();
+Console.WriteLine(m);
+
+var m1 = new Matrix2d(1,2,3,4);
+Console.WriteLine(m1);
--- Matrix2d/Matrix2dLib/matrix2d.cs
+++ Matrix2d/Matrix2dLib/matrix2d.cs
@@ -1,6 +1,7 @@
-﻿namespace Matrix2dLib
+﻿#nullable disable
+namespace Matrix2dLib
 {
-    public class Matrix2d
+    public class Matrix2d : IEquatable<Matrix2d>
     {
         int a, b, c, d; // private fields
         /*
@@ -18,17 +19,87 @@
         public Matrix2d() : this(1, 0, 0, 1) { }
 
         //publiczne stałe
-        public static  Matrix2d Id
+        public static Matrix2d Id
         {
             get { return new Matrix2d(); }
         }
 
         //macierz zerowa
-        public static  Matrix2d Zero
+        public static Matrix2d Zero
         {
             get { return new Matrix2d(0, 0, 0, 0); }
         }
 
+        // Przesłonięcie metody ToString()
+        public override string ToString()
+        {
+            return $"[[{a}, {b}], [{c}, {d}]]";
+        }
+
+        // Przesłonięcie metody Equals()
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Matrix2d);
+        }
+        public bool Equals(Matrix2d other)
+        {
+            if (other is null)
+                return false;
+
+            return a == other.a &&
+                b == other.b &&
+                c == other.c &&
+                d == other.d;
+        }
+
+        // Przeciążenie operatora ==
+        public static bool operator ==(Matrix2d left, Matrix2d right)
+            => left.Equals(right);
+
+        // Przeciążenie operatora !=
+        public static bool operator !=(Matrix2d left, Matrix2d right)
+            => !(left == right);
+        // Przesłonięcie metody GetHashCode()
+        public override int GetHashCode()
+           => HashCode.Combine(a, b, c, d);
+
+
+        // dodawanie macierzy
+        public static Matrix2d operator +(Matrix2d left, Matrix2d right)
+            => new Matrix2d(left.a + right.a, left.b + right.b,
+                            left.c + right.c, left.d + right.d);
+
+
+        // odejmowanie macierzy
+        public static Matrix2d operator -(Matrix2d left, Matrix2d right)
+           => new Matrix2d(left.a - right.a, left.b - right.b,
+                           left.c - right.c, left.d - right.d);
+
+
+        // mnożenie macierzy
+        public static Matrix2d operator *(Matrix2d left, Matrix2d right)
+            => new Matrix2d(
+                left.a * right.a + left.b * right.c,
+                left.a * right.b + left.b * right.d,
+                left.c * right.a + left.d * right.c,
+                left.c * right.b + left.d * right.d);
+
+
+        // mnożenie macierzy przez liczbę 
+        public static Matrix2d operator *(int k, Matrix2d macierz)
+            => new Matrix2d(k * macierz.a, k * macierz.b,
+                            k * macierz.c, k * macierz.d);
+
+        public static Matrix2d operator *(Matrix2d macierz, int k)
+           => k * macierz;
+
+        // jednoargumentowy operator zmiany znaku
+        public static Matrix2d operator -(Matrix2d macierz)
+            => new Matrix2d(-macierz.a, -macierz.b,
+                            -macierz.c, -macierz.d);
+        // operację transpozycji macierzy 
+        public static Matrix2d Transpose(Matrix2d m)
+            => new Matrix2d(m.a, m.c, m.b, m.d);
     }
 }
 
